204dfce0241bfa4929564335529902a22d36de14
Brandon Miller <brandonmiller@unknown84383564ec6c.gateway.2wire.net>
2013-08-22 08:00:21 -0400

    Added Error Checking

--- test.js
+++ test.js
@@ -4,31 +4,91 @@ $(document).ready(function(){
 
 	
 		var firstNameValue = $('.firstName').val();
+		
+		if (firstNameValue == "") {
+			$('#FirstName').text("Error Must Provide First Name");
+			$('#FirstName').css({color:"red"})
+		}	else{
 		$('#FirstName').text(firstNameValue);
-		if (firstNameValue == undefined) {
-			firstNameValue="Error";
-		}
+		$('#FirstName').css({color:"black"})
+			}
+			
 
 		var lastNameValue = $('.lastName').val();
+		
+		if (lastNameValue == "") {
+			$('#LastName').text("Error Must Provide Last Name");
+			$('#LastName').css({color:"red"})
+		}	else{
 		$('#LastName').text(lastNameValue);
+		$('#LastName').css({color:"black"})
+			}
 
 		var emailValue = $('.email').val();
-		$('#Email').text(emailValue);
+
+		if (emailValue == "") {
+			$('#Email').text("Error Must Provide Email Address");
+			$('#Email').css({color:"red"})
+		}	
+		else{
+			$('#Email').text(emailValue);
+			$('#Email').css({color:"black"})
+		}
 
 		var homeTownValue = $('.homeTown').val();
-		$('#HomeTown').text(homeTownValue);
+		
+		if (homeTownValue == "") {
+			$('#HomeTown').text("Error Must Provide a Home Town");
+			$('#HomeTown').css({color:"red"})
+		}	
+		else{
+			$('#HomeTown').text(homeTownValue);
+			$('#HomeTown').css({color:"black"})
+		}
 
 		var homeStateValue = $('.homeState').val();
-		$('#HomeState').text(homeStateValue);
+		
+		if (homeStateValue == "") {
+			$('#HomeState').text("Error Must Provide a Home State");
+			$('#HomeState').css({color:"red"})
+		}	
+		else{
+			$('#HomeState').text(homeStateValue);
+			$('#HomeState').css({color:"black"})
+		}
 
 		var monthValue = $('.month').val();
-		$('#Month').text(monthValue);
+		
+		if (monthValue == "") {
+			$('#Month').text("Error Must Provide Month of Birth");
+			$('#Month').css({color:"red"})
+		}	
+		else{
+			$('#Month').text(monthValue);
+			$('#Month').css({color:"black"})
+		}
 
 		var dateValue = $('.date').val();
-		$('#Date').text(dateValue);
+		
+		if (dateValue == "") {
+			$('#Date').text("Error Must Provide a Date of Birth");
+			$('#Date').css({color:"red"})
+		}	
+		else{
+			$('#Date').text(dateValue);
+			$('#Date').css({color:"black"})
+		}
 
 		var yearValue = $('.year').val();
-		$('#Year').text(yearValue);
+		
+		if (yearValue == "") {
+			$('#Year').text("Error Must Provide a Year of Birth");
+			$('#Year').css({color:"red"})
+		}	
+		else{
+			$('#Year').text(yearValue);
+			$('#Year').css({color:"black"})
+		}
 
 		$('.icon').css({opacity:"1"})
 		$('h4').css({opacity:"1"})
