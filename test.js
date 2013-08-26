479e267ac27bb2c5a63309e081b765c5d2298526
Brandon Miller <brandonmiller@unknown84383564ec6c.gateway.2wire.net>
2013-08-25 21:21:21 -0400

    Added keyup and Alerts

--- test.js
+++ test.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
 
 	$('.large').click(function() {
-
-	
+		var counter = 0;
+		
 		var firstNameValue = $('.firstName').val();
 		
 		if (firstNameValue == "") {
 			$('#FirstName').text("Error Must Provide First Name");
 			$('#FirstName').css({color:"red"})
+			alert('No First Name.');
+			counter=1;
 		}	else{
 		$('#FirstName').text(firstNameValue);
-		$('#FirstName').css({color:"black"})
+		$('#FirstName').css({color:"black"});
 			}
 			
 
@@ -19,6 +21,8 @@ $(document).ready(function(){
 		if (lastNameValue == "") {
 			$('#LastName').text("Error Must Provide Last Name");
 			$('#LastName').css({color:"red"})
+			alert('No Last Name.');
+			counter=1;
 		}	else{
 		$('#LastName').text(lastNameValue);
 		$('#LastName').css({color:"black"})
@@ -29,6 +33,8 @@ $(document).ready(function(){
 		if (emailValue == "") {
 			$('#Email').text("Error Must Provide Email Address");
 			$('#Email').css({color:"red"})
+			alert('No Email Address.');
+			counter=1;
 		}	
 		else{
 			$('#Email').text(emailValue);
@@ -40,6 +46,8 @@ $(document).ready(function(){
 		if (homeTownValue == "") {
 			$('#HomeTown').text("Error Must Provide a Home Town");
 			$('#HomeTown').css({color:"red"})
+			alert('No Home Town.');
+			counter=1;
 		}	
 		else{
 			$('#HomeTown').text(homeTownValue);
@@ -51,6 +59,8 @@ $(document).ready(function(){
 		if (homeStateValue == "") {
 			$('#HomeState').text("Error Must Provide a Home State");
 			$('#HomeState').css({color:"red"})
+			alert('No Home State.');
+			counter=1;
 		}	
 		else{
 			$('#HomeState').text(homeStateValue);
@@ -62,6 +72,8 @@ $(document).ready(function(){
 		if (monthValue == "") {
 			$('#Month').text("Error Must Provide Month of Birth");
 			$('#Month').css({color:"red"})
+			alert('No Birth Month.');
+			counter=1;
 		}	
 		else{
 			$('#Month').text(monthValue);
@@ -73,6 +85,8 @@ $(document).ready(function(){
 		if (dateValue == "") {
 			$('#Date').text("Error Must Provide a Date of Birth");
 			$('#Date').css({color:"red"})
+			alert('No Birth Date.');
+			counter=1;
 		}	
 		else{
 			$('#Date').text(dateValue);
@@ -84,6 +98,8 @@ $(document).ready(function(){
 		if (yearValue == "") {
 			$('#Year').text("Error Must Provide a Year of Birth");
 			$('#Year').css({color:"red"})
+			alert('No Birth Year.');
+			counter=1;
 		}	
 		else{
 			$('#Year').text(yearValue);
@@ -93,5 +109,51 @@ $(document).ready(function(){
 		$('.icon').css({opacity:"1"})
 		$('h4').css({opacity:"1"})
 	})
+
+
+	$('.firstName').keyup(function(event) {
+    newText = event.target.value;
+    $('#FirstName').text(newText);
+  });
+	
+	$('.lastName').keyup(function(event) {
+    newText = event.target.value;
+    $('#LastName').text(newText);
+  });
+
+	$('.email').keyup(function(event) {
+    newText = event.target.value;
+    $('#Email').text(newText);
+  });
+
+	$('.homeTown').keyup(function(event) {
+    newText = event.target.value;
+    $('#HomeTown').text(newText);
+  });
+
+	$('.homeState').keyup(function(event) {
+    newText = event.target.value;
+    $('#HomeState').text(newText);
+  });
+
+	$('.month').keyup(function(event) {
+    newText = event.target.value;
+    $('#Month').text(newText);
+  });
+
+	$('.date').keyup(function(event) {
+    newText = event.target.value;
+    $('#Date').text(newText);
+  });
+
+	$('.year').keyup(function(event) {
+    newText = event.target.value;
+    $('#Year').text(newText);
+  });
+
+
+
+
+
 })
 
