278c3e330556d4a2c8356c212aca849da7f633cf
Brandon Miller <brandonmiller@unknown84383564ec6c.gateway.2wire.net>
2013-08-28 14:45:42 -0400

    Added four more tests to test individual missing fields and correct data storage

--- spec/Input-spec.js
+++ spec/Input-spec.js
@@ -5,13 +5,31 @@ describe ("Social Input", function(){
 			$('input').val('')
 		})
 
-		it("should not run if an entry is blank so the users list should be Undefined", function(){
+		it("not running if all entries are blank so the users list should be Undefined", function(){
 			$('.large').click()
 			expect($('userList ul li')).toBeUndefined
 
 		})
 
-		it("should not update the array with values when and entry is blank", function(){
+		it("not update the array with values when all entries are blank", function(){
+			expect(dataCollect).toBeUndefined
+
+		})
+
+		it("not running if any entry is blank so the users list should be Undefined", function(){
+			$('.firstName').val('Johnny');
+			$('.email').val('jcistheman@hotmail.com');
+			$('.homeTown').val('TR');
+			$('.homeState').val('SC');
+			$('.month').val('January');
+			$('.date').val('1');
+			$('.year').val('1923');
+			$('.large').click()
+			expect($('userList ul li')).toBeUndefined
+
+		})
+
+		it("not update the array with values when one entry is blank", function(){
 			expect(dataCollect).toBeUndefined
 
 		})
@@ -36,9 +54,16 @@ describe ("Social Input", function(){
 
 		it("successfully records the object into the array dataCollect", function(){
 			expect(dataCollect[0].firstName).toBe('Johnny')
+			expect(dataCollect[0].lastName).toBe('Camper')
 
 		})
 
+		it("update the user list on the page with the new addition", function(){
+			expect($('.userList ul li').last().text()).toBe('Johnny Camper');
+		})
+
+		it("only run the number of times of successful data entry")
+			expect(count).toEqual(1)
 
 	})
 
